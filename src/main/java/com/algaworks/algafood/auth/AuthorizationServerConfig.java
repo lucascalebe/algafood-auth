69edbc00d3e9bb84f9cb116e0e72e843c9c9f7d1
Lucas Calebe <lucascalebe07@hotmail.com>
2021-11-30 23:16:37 -0300

    22.14. Configurando a validade e não reutilização de Refresh Tokens

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -31,7 +31,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                     .secret(passwordEncoder.encode("web123"))
                     .authorizedGrantTypes("password", "refresh_token")
                     .scopes("write","read")
-                    .accessTokenValiditySeconds(60 * 60 * 6)
+                    .accessTokenValiditySeconds(6 * 60 * 60)
+                    .refreshTokenValiditySeconds(60  * 24 * 60 * 60)
                 .and()
                 .withClient("checkToken")
                     .secret(passwordEncoder.encode("check123"));
@@ -47,6 +48,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.
                 authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false);
     }
 }
