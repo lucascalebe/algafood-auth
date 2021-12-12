bec5aa61619adc31cfa3b75405cca92c34038c62
Lucas Calebe <lucascalebe07@hotmail.com>
2021-12-11 22:55:04 -0300

    23.19. Carregando as permissões concedidas na autenticação

--- src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
+++ src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -24,4 +26,8 @@ public class Usuario {
     @Column(nullable = false)
     private String senha;
 
+    @ManyToMany
+    @JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id")
+            , inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private Set<Grupo> grupos = new HashSet<>();
 }
